a3009ec1ee6d6720c45187affa9600a472294c56
Sebastian <theseb242@gmail.com>
2023-03-14 16:38:56 -0500

    unas cuantas validaciones y cambios ligeros de frontend

--- main.js
+++ main.js
@@ -1,5 +1,34 @@
-const cantidad = document.querySelector(".cantidad");
 
-if(cantidad > 30){
-    alert("la cantidad no puede ser mayor a 30")
+var cantidad = document.querySelector(".cantidad").value;
+var porcdescuento = document.querySelector(".descuento").value;
+var precioUni = document.querySelector(".unitario").value;
+var costimporte = document.querySelector(".importe").value;
+var envio = document.querySelector(".envio")
+envio.addEventListener("click", calcularTotal)
+
+
+function validar(){
+
+    var cantidad = document.querySelector(".cantidad").value;
+    if(cantidad > 30 || cantidad< 1){
+
+        alert("la cantidad ingresada no es valida")
+    }
+
+    var descuento = document.querySelector(".descuento").value;
+    if(descuento > 50 || descuento < 1){
+
+        alert("el descuento ingresado no es valido")
+    }
+
+}
+
+function calcularTotal(){
+
+    let total = cantidad * precioUni;
+    let descuento = total * (porcdescuento/100)
+    total += costimporte;
+    total = total - descuento;
+
+    alert(total);
 }
