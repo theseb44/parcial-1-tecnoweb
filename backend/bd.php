40cd798a5e187b0cbd88c9ad1aa9c7e3e2bbc8f2
jose daniel <josedaniel12241a@gmail.com>
2023-03-19 12:04:20 -0500

    backend

---
+++ backend/bd.php
@@ -0,0 +1,12 @@
+<?php
+$servername="localhost";
+$username="root";
+$password="";
+$database="proyecto-web-1";
+
+
+$conexion = new mysqli($servername, $username, $password, $database ) or die ("error conexion");
+
+echo "conectado"
+
+?>
